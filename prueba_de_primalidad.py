e478b7aff2f5ac65f2df26287a70e97f7c1dc7dd
gmorales254 <55248854+gmorales254@users.noreply.github.com>
2022-02-08 13:24:29 -0300

    Create prueba_de_primalidad.py

---
+++ prueba_de_primalidad.py
@@ -0,0 +1,20 @@
+def es_primo(numero):
+    contador = 0
+
+    for i in range(1, numero += 1):
+        if i == 1 or i == numero:
+            continue
+        if numero += 1
+        else:
+            pass
+
+def run():
+    numero = int(input("Escribe un numero: "))
+    if es_primo(numero):
+        print("Es primo")
+    else:
+        print("No es primo")
+
+
+if __name__ == "__main__":
+    run()
